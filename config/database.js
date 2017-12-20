420bb27a7985de90fd43be90f06419175020dc89
Tarasov V <vovatarasov6991@gmail.com>
2017-12-20 17:53:54 +0200

    setup database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'root123'
+        'host': 'us-cdbr-iron-east-05.cleardb.net',
+        'user': 'b4eb757e5cf4d1',
+        'password': '7ede8b1c'
     },
-	'database': 'football',
+	'database': 'heroku_d9e1584a9d6e956',
     'users_table': 'users',
     'scores_table':'scores'
 };
